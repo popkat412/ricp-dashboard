b1403c561a7783bd58755b753bb67dbff05180c1
wangy412 <wangyunze412@gmail.com>
2022-07-05 21:36:12 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="sticky top-0 z-30 bg-gray-900">
+    <div
+      class="
+        flex
+        items-center
+        justify-between
+        border-b border-gray-800
+        px-4
+        py-5
+        bg-opacity-50
+        backdrop-blur backdrop-filter
+      "
+    >
+      <div class="text-slate-50">RICP Point System</div>
+    </div>
+  </header>
+  <div class="bg-gray-900"></div>
+</template>
