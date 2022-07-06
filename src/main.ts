255a0ba3301d5e86db8180e7f45fa8abf3976843
wangy412 <wangyunze412@gmail.com>
2022-07-06 21:22:51 +0800

    feat: add firebase config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+// Import the functions you need from the SDKs you need
+import { initializeApp as initializeFirebaseApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import App from "./App.vue";
 import "./index.css";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQqYtaXieMU9e0ZEVxIkmMh1ieR3aKbzY",
+  authDomain: "ricp-points.firebaseapp.com",
+  projectId: "ricp-points",
+  storageBucket: "ricp-points.appspot.com",
+  messagingSenderId: "80997449627",
+  appId: "1:80997449627:web:ffd84151f74df1eed66b3e",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeFirebaseApp(firebaseConfig);
 
 const pinia = createPinia();
 const app = createApp(App);
