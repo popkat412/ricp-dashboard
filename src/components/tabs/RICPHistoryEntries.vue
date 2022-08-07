4e2398110f5775d3f7f6912f5294e5c5965c4248
popkat412 <wangyunze412@gmail.com>
2022-08-07 09:45:07 +0800

    feat: improve history ui

--- src/components/tabs/RICPHistoryEntries.vue
+++ src/components/tabs/RICPHistoryEntries.vue
@@ -1,40 +1,48 @@
 <template>
-  <table class="table-auto w-full overflow-x-auto min-w-[640px]">
-    <thead class="text-left">
-      <tr>
-        <th>Time</th>
-        <th>Change</th>
-        <th v-if="'memberName' in $props.entries[0]">Name</th>
-        <th>Message</th>
-        <th>Admin</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(entry, idx) in $props.entries" :key="idx" class="h-10">
-        <td class="flex space-x-5 h-10 items-center p-0">
-          <div>
-            {{ formatTimestamp(entry.timestamp) }}
-          </div>
-          <div class="w-5 self-stretch border-l-2 border-white relative">
-            <div
-              class="rounded-full w-3 h-3 border-white border-2 bg-gray-500 absolute top-2/4 left-0"
-              style="transform: translate(calc(-50% - 0.5px), -50%)"
-            ></div>
-          </div>
-        </td>
-        <td>
-          <strong>{{ formatChange(entry.change) }}</strong>
-        </td>
-        <td v-if="'memberName' in entry">
-          <em>{{ entry.memberName }}</em>
-        </td>
-        <td>{{ entry.message }}</td>
-        <td>
-          <em>{{ entry.adminName }}</em>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="w-full overflow-x-scroll">
+    <table class="table-auto w-full min-w-[700px]">
+      <thead class="text-left bg-gray-700">
+        <tr>
+          <th class="py-3 px-2">Time</th>
+          <th class="py-3 pr-2">Change</th>
+          <th class="py-3 pr-2" v-if="'memberName' in $props.entries[0]">
+            Name
+          </th>
+          <th class="py-3 pr-2">Message</th>
+          <th class="py-3 pr-2">Admin</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(entry, idx) in $props.entries"
+          :key="idx"
+          class="h-10 border-b border-b-white/10"
+        >
+          <td class="flex space-x-5 h-10 items-center">
+            <div>
+              {{ formatTimestamp(entry.timestamp) }}
+            </div>
+          </td>
+
+          <td>
+            <strong>{{ formatChange(entry.change) }}</strong>
+          </td>
+
+          <td v-if="'memberName' in entry">
+            <em>{{ entry.memberName }}</em>
+          </td>
+
+          <td>
+            {{ entry.message }}
+          </td>
+
+          <td>
+            <em>{{ entry.adminName }}</em>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup lang="ts">
